2a40e2c49a4c63da1b43d7ff78900fed9f6177d3
Serhii Tkachenko <x.ray868@gmail.com>
2021-08-05 00:34:53 +0300

    Update

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ class CountdownTimer {
     }, 1000);
   }
 
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+
   getTime(time) {
     const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
     const hours = this.pad(
@@ -26,10 +30,6 @@ class CountdownTimer {
     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
     return { days, hours, mins, secs };
-  }
-
-  pad(value) {
-    return String(value).padStart(2, "0");
   }
 }
 
